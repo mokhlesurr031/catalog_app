b496cce7aff374e4bd64dd03321176966b66dd03
Mokhlesur Rahman <mokhlesurr031@gmail.com>
2023-03-25 20:00:08 +0600

    day2

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  int days = 25;
+  String name = "Mahin";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of Flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
