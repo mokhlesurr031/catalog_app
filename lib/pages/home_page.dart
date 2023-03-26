526839c90f26233f6b17ee78066b7186a3112adb
Mokhlesur Rahman <mokhlesurr031@gmail.com>
2023-03-26 10:12:19 +0600

    day13- local json data loaded, json file added and configured in pubspec

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,33 @@
+import "dart:convert";
+
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:flutter_catalog/models/catalog.dart";
 import "package:flutter_catalog/widgets/drawer.dart";
 
 import "../widgets/item_widget.dart";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int days = 25;
   String name = "Mahin";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
 
   @override
   Widget build(BuildContext context) {
