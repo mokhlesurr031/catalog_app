7b127ae2911220daaa8e0ba0bd9fcc97e5e49aa7
Mokhlesur Rahman <mokhlesurr031@gmail.com>
2023-03-26 07:59:56 +0600

    day11- app icon changed, catalog sample data added, theme issue fixed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,7 @@
+import 'dart:ffi';
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String? description;
   final num price;
@@ -14,3 +16,15 @@ class Item {
       this.color,
       this.image});
 }
+
+final product = [
+  Item(
+    id: 1,
+    name: "iPhone 12 Pro",
+    price: 999,
+    description: "Apple iPhone is very good",
+    color: "#33505a",
+    image:
+        "https://www.bing.com/images/search?view=detailV2&ccid=nPiQbY%2bK&id=1701BBA043EFEBBEBA3B2E47C22A8F5AAB911754&thid=OIP.nPiQbY-KhdWclK_oceQeLQHaEL&mediaurl=https%3a%2f%2ffdn.gsmarena.com%2fimgroot%2fnews%2f20%2f10%2fiphone-12-roundup%2f-1200%2fgsmarena_001.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.9cf8906d8f8a85d59c94afe871e41e2d%3frik%3dVBeRq1qPKsJHLg%26pid%3dImgRaw%26r%3d0&exph=676&expw=1200&q=iphone+12+pro+max&simid=608045882162230194&FORM=IRPRST&ck=2768E79D96CBC1C02CFBFA1A2315AC32&selectedIndex=50",
+  )
+];
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,10 +1,10 @@
 import "package:flutter/material.dart";
-import "package:google_fonts/google_fonts.dart";
+// import "package:google_fonts/google_fonts.dart";
 
 class MyTheme {
   static ThemeData get lightTheme => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      // fontFamily: GoogleFonts.lato().fontFamily,
       appBarTheme: const AppBarTheme(
           color: Colors.white,
           elevation: 0.0,
@@ -19,7 +19,7 @@ class MyTheme {
 
   static ThemeData get regularTheme => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      // fontFamily: GoogleFonts.lato().fontFamily,
       appBarTheme: const AppBarTheme(
         color: Colors.deepPurple,
         elevation: 0.0,
